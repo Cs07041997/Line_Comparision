79110f3c3d09bdfaf6f6671e54e128255d934240
Cs07041997 <84439789+Cs07041997@users.noreply.github.com>
2021-07-01 00:32:22 +0530

    LineComparision

---
+++ linecommand/Uc1LineCommand.java
@@ -0,0 +1,25 @@
+package com.bridgelabz.linecommand;
+
+import java.util.Scanner;
+
+public class Uc1LineCommand {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter x co-ordinate of First Point: ");
+		int x1 = scanner.nextInt();
+		System.out.print("Enter y co-ordinate of First Point: ");
+		int y1 = scanner.nextInt();
+		System.out.print("Enter x co-ordinate of Second Point: ");
+		int x2 = scanner.nextInt();
+		System.out.print("Enter y co-ordinate of Second Point: ");
+		int y2 = scanner.nextInt();
+		scanner.close();
+
+		double lenghtOfLine = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.printf("Length of End Point is : %.2f %n", lenghtOfLine);
+
+
+	}
+
+}
